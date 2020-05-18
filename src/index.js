f65b4f43612a897bbe72c4696e461b79ba3ccdf3
yocharitha <yocharitha@gmail.com>
2020-05-18 20:05:24 +0530

    fix(index.js): add a exclamination mark
    fix description long

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import classNames from "./Sample.module.scss";
 
 function SampleComponent({ children }) {
-  return <div className={classNames.wrapper}>{children}</div>;
+  return <div className={classNames.wrapper}>{children}!</div>;
 }
 
 export default SampleComponent;
